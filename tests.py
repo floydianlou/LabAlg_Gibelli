013ba6275438f8ad9b080259ba2ef810eb3f4a9c
floydianlou <alice.gibelli@stud.unifi.it>
2023-08-13 13:44:35 +0200

    add tests file

---
+++ tests.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import utility
+from algorithm_class import LCSalgorithm
+from bruteforce import bruteforce_lcs
+from recursive import recursive_lcs
+from memoization import memoization_recursive_lcs
+from bottomup import bottomup_lcs
+
+algorithms = [LCSalgorithm('BruteForce', 16, utility.generate_random_string, bruteforce_lcs),
+              LCSalgorithm('Recursive Mid Case', 16, utility.generate_random_string, recursive_lcs),
+              LCSalgorithm('Memoization', 250, utility.generate_random_string, memoization_recursive_lcs),
+              LCSalgorithm('BottomUp', 250, utility.generate_random_string, bottomup_lcs),
+              LCSalgorithm('Recursive Best Case', 16, utility.bestcase_strings, recursive_lcs),
+              LCSalgorithm('Recursive Worst Case', 16, utility.worstcase_strings, recursive_lcs)]
+
+############### TEST AREA ################
+
+strings_tests = 10
+total_results = []
+
+for algo in algorithms:
+    total_results.extend(utility.test_lcs_algorithms(strings_tests, algo))
+
+export_df = pd.DataFrame(total_results)
+
+excel_file_path = "C:/Users/acgib/Desktop/univ/algorithms laboratory/NEW TESTS.xlsx"
+export_df.to_excel(excel_file_path, index=False, engine='openpyxl')
